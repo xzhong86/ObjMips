3dcb8a972a994d50d9fe2c042ec55739f41428a8
zhiping zhong <zpzhong@ingenic.cn>
2012-04-16 19:53:55 +0800

    memory test, dma fw

---
+++ fw-tst/dma.c
@@ -0,0 +1,135 @@
+
+
+#define DMA_BASE	0xB3420000
+
+#define CH_DSA	0x0
+#define CH_DTA	0x4
+#define CH_DTC	0x8
+#define CH_DRT	0xC
+#define CH_DCS	0x10
+#define CH_DCM	0x14
+#define CH_DDA	0x18
+#define CH_DSD	0x1C
+
+#define DMAC	0x1000
+#define DIRQP	0x1004
+#define DDR	0x1008
+#define DDRS	0x100C
+
+
+#define DMAC_HLT	BIT(3)
+#define DMAC_AR		BIT(2)
+#define DMAC_EN		BIT(0)
+
+#define DCS_NDESC	BIT(31)
+#define DCS_AR		BIT(4)
+#define DCS_TT		BIT(3)
+#define DCS_HLT		BIT(2)
+#define DCS_CTE		BIT(0)
+
+#define DCM_SAI		BIT(23)
+#define DCM_DAI		BIT(22)
+#define DCM_SP_MSK	(0x3 << 14)
+#define DCM_SP_16	BIT(15)
+#define DCM_SP_8	BIT(14)
+#define DCM_DP_MSK	(0x3 << 12)
+#define DCM_DP_16	BIT(13)
+#define DCM_DP_8	BIT(12)
+#define DCM_TSZ_MSK	(0x7 << 8)
+#define DCM_TSZ_SHF	8
+#define DCM_TIE		BIT(1)
+#define DCM_LINK	BIT(0)
+
+#define TEST_SIZE	(16*1024)
+struct my_data {
+	int times;
+	int *mem;
+	void *iomem;
+	phy_t phy;
+}
+
+static int dma_prepare(struct fw_dev *dev)
+{
+	struct my_data *md;
+
+	md = malloc(sizeof(*md));
+	if (!md)
+		return -1;
+	md->mem = mem_alloc(TEST_SIZE);
+	if (!md->mem) {
+		free(md);
+		return -1;
+	}
+	md->phy = mem_get_phy(md->mem);
+	md->iomem = ioremap(DMA_BASE, 0x1000);
+	dev->priv = md;
+	return 0;
+}
+
+static int dma_start(struct fw_dev *dev)
+{
+	struct my_data *md = dev->priv;
+	int i;
+
+	md->times ++;
+	for (i = 0; i < TEST_SIZE/8; i++) 
+		md->mem[i] = rand();
+	
+	return 0;
+}
+
+static void dma_ask(struct fw_dev *dev)
+{
+	struct my_data *md = dev->priv;
+	unsigned int *p;
+
+	p = md->mem + (BSIZE/4) * md->pos;
+	memcpy(p, md->mem, BSIZE);
+	p[0] = get_int_hash(p+1, BSIZE/4-1);
+	if (++md->pos >= MSIZE/BSIZE)
+		fw_finish(dev);
+}
+
+static chk_t dma_check(struct fw_dev *dev)
+{
+	struct my_data *md = dev->priv;
+	int i;
+
+	if (md->pos != MSIZE/BSIZE)
+		return CHK_FAILED;
+	for (i = 0; i < MSIZE/BSIZE; i++)
+		if (md->mem[0] != md->mem[i*BSIZE/4])
+			return CHK_FAILED;
+
+	if (md->times ++ > 10)
+		return CHK_FINISHED;
+	return CHK_PASSED;
+}
+
+static void dma_stop(struct fw_dev *dev)
+{
+	struct my_data *md = dev->priv;
+
+	if (md->times <= 10)
+		printk("no, i stopped at %d\n", md->times);
+	else
+		printk("o, i stopped\n");
+	mem_free(md->mem);
+	free(md);
+}
+
+static struct fw_ops dma_ops = {
+	.prepare = dma_prepare,
+	.start = dma_start,
+	.ask = dma_ask,
+	.check = dma_check,
+	.stop = dma_stop,
+};
+
+static int init_dev(void)
+{
+	if (!fwdev_register("dma", &dma_ops))
+		return -1;
+	return 0;
+}
+fw_initcall(init_dev,0);
--- test/memory.c
+++ test/memory.c
@@ -53,3 +53,100 @@ static int do_mem(int argc,char *argv[])
 	return 0;
 }
 DEF_COMMAND("mem",do_mem,do_mem_help);
+
+#include <mem.h>
+#include <pmon.h>
+#include <test/test-head.h>
+static void *t_mem;
+static int t_size;
+static int t_tmp;
+static int mread(void)
+{
+	int i,tmp = 0,*p = t_mem;
+
+	for (i = 0; i < t_size/4; i += 8, p += 8) {
+		tmp ^= p[0]; tmp ^= p[1]; tmp ^= p[2]; tmp ^= p[3];
+		tmp ^= p[4]; tmp ^= p[5]; tmp ^= p[6]; tmp ^= p[7];
+	}
+	t_tmp = tmp;
+	return t_size;
+}
+static int mwrite(void)
+{
+	int i,*p = t_mem;
+
+	for (i = 0; i < t_size/4; i += 8, p += 8) {
+		p[0] = i; p[1] = i; p[2] = i; p[3] = i;
+		p[4] = i; p[5] = i; p[6] = i; p[7] = i;
+	}
+	return t_size;
+}
+static int jread(void)
+{
+	int i,tmp = 0,*p = t_mem;
+
+	for (i = 0; i < t_size/4; i += 16, p += 16) {
+		tmp ^= p[0]; tmp ^= p[4]; tmp ^= p[8]; tmp ^= p[12];
+	}
+	t_tmp = tmp;
+	return t_size;
+}
+static int jwrite(void)
+{
+	int i,*p = t_mem;
+
+	for (i = 0; i < t_size/4; i += 16, p += 16) {
+		p[0] = i; p[4] = i; p[8] = i; p[12] = i;
+	}
+	return t_size;
+}
+static int mset(void)
+{
+	memset(t_mem, 0x5a, t_size);
+	return t_size;
+}
+static int mcopy(void)
+{
+	memcpy(t_mem, t_mem + t_size/2, t_size/2);
+	return t_size/2;
+}
+static void test_case(int (*fun)(void), char *desc)
+{
+	unsigned long tmp,cyc,cycle = 0;
+	int times = 0;
+	unsigned long size = 0;
+
+	pmon_prepare(PMON_EVENT_CYCLE);
+	do {
+		pmon_clear_cnt();
+		pmon_start();
+
+		size += fun();
+
+		pmon_stop();
+		pmon_get_cnt32(tmp,cyc);
+		cycle += cyc;
+		times ++;
+		if (cycle > 400*1000000)
+			break;
+	} while(1);
+
+	tmp = size / (cycle / 1000000);
+	printk("%10s %5ld kB in 1 Mcycle. (total %ld kB, %ld cycle)\n",
+	       desc, tmp/1024, size/1024, cycle);
+}
+static int memory(void)
+{
+	t_size = 8*1024*1024;
+	t_mem = mem_alloc(t_size);
+	test_case(mread, "seq read");
+	test_case(mwrite, "seq write");
+	test_case(jread, "j4w read");
+	test_case(jwrite, "j4w write");
+	test_case(mset, "std write");
+	test_case(mcopy, "std copy");
+
+	mem_free(t_mem);
+	return 0;
+}
+DEF_TEST(memory, 30, TESTF_REPEAT);
